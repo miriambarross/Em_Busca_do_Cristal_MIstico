7ebb9298988c931cdaf228869a0899b3e830d744
miriambarross <148462345+miriambarross@users.noreply.github.com>
2024-09-18 14:57:50 -0300

    Adicionando arquivos do projeto.

---
+++ script.js
@@ -0,0 +1,11 @@
+const avancar = document.querySelectorAll('.btn-proximo');
+
+avancar.forEach(button => {
+    button.addEventListener('click', function() {
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    });
+});
